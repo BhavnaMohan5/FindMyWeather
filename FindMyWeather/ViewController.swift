54ecd1fa2dc4f76a3639cbfe3c6a4fb3e7f6666b
Bhavna <bhavnamohan5@gmail.com>
2019-01-15 23:07:58 +0530

    Fetch and display location name,print weather data in console and solved an error - does not conform to ats policy by putting a fix in info.plist"

--- FindMyWeather/ViewController.swift
+++ FindMyWeather/ViewController.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 import CoreLocation
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController,CLLocationManagerDelegate {
 
     //IBOutlets and Variable Declaration :
-    @IBOutlet weak var CurrentLocation: UILabel!
+    @IBOutlet weak var currentLocation: UILabel!
     @IBOutlet weak var currentWeatherCondition: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
     
     let locationManager = CLLocationManager()
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +37,24 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         locationManager.stopUpdatingLocation()
         locationManager.delegate = nil
         print("latitude : \(location.coordinate.latitude) and longitude : \(location.coordinate.longitude)")
+        let params : [String : String] = ["lat" : String(location.coordinate.latitude),"lon" : String(location.coordinate.longitude),"appid" : APP_ID]
+        getDataFromOpenWeatherMaps(weatherURL : WEATHER_URL,parameters: params)
     }
+    }
+    
+    func getDataFromOpenWeatherMaps(weatherURL : String,parameters : [String : String]) {
+        Alamofire.request(weatherURL, method: .get, parameters: parameters).responseJSON {
+            response in
+                if response.result.isSuccess {
+                    let weatherJSON = JSON(response.result.value!)
+                    self.currentLocation.text = weatherJSON["name"].string
+                    print(weatherJSON)
+                }
+                else {
+                    print("Error : \(response.result.error!)")
+                    self.currentLocation.text = "Connection Issues"
+            }
+        }
     }
 
 
