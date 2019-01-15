00f033944e5fa1375bfadac580695f77347c0c17
Bhavna <bhavnamohan5@gmail.com>
2019-01-15 21:46:14 +0530

    Set the basic UI,Conformed to CLLocation Manager,Ask user's permission for location,Detect users location and print latitude and longitude on the console

--- FindMyWeather/ViewController.swift
+++ FindMyWeather/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
 
+    //IBOutlets and Variable Declaration :
+    @IBOutlet weak var CurrentLocation: UILabel!
+    @IBOutlet weak var currentWeatherCondition: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    //MARK : Location Manager didUpdate Location
+   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+    if location.horizontalAccuracy > 0 {
+        locationManager.stopUpdatingLocation()
+        locationManager.delegate = nil
+        print("latitude : \(location.coordinate.latitude) and longitude : \(location.coordinate.longitude)")
+    }
     }
 
 
