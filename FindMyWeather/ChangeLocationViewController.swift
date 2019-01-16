9d58b1bbd996185d2d2e8e68f5321aef545f99e1
Bhavna Mohan <bhavnamohan@Bhavnas-MacBook-Air.local>
2019-01-16 15:40:45 +0530

    Implemented Second View Controller that lets user chnage location acc to lat,lon or city and fetch and display weather data

---
+++ FindMyWeather/ChangeLocationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChangeLocationViewController.swift
+//  FindMyWeather
+//
+//  Created by Bhavna Mohan on 16/01/19.
+//  Copyright © 2019 Bhavna Mohan. All rights reserved.
+//
+
+import UIKit
+
+class ChangeLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FindMyWeather/WeatherDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherDataModel.swift
+//  FindMyWeather
+//
+//  Created by Bhavna Mohan on 15/01/19.
+//  Copyright © 2019 Bhavna Mohan. All rights reserved.
+//
+
+import Foundation
